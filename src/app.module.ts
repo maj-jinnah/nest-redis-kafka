4adada65917a5f7e42849a91548021cf9101e108
maj-jinnah <maj.jinnah1999@gmail.com>
2025-12-10 10:52:34 +0600

    all services is created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import configuration from './config/configuration';
 import { AuthModule } from './infrastructure/auth/auth.module';
 import { DatabaseModule } from './infrastructure/database/database.module';
-import { ConfigModule } from '@nestjs/config';
-import configuration from './config/configuration';
+import { KafkaModule } from './infrastructure/kafka/kafka.module';
+import { AnalyticModule } from './modules/analytic/analytic.module';
+import { EmailModule } from './modules/email/email.module';
+import { OrderModule } from './modules/order/order.module';
+import { PaymentModule } from './modules/payment/payment.module';
 
 @Module({
   imports: [
@@ -15,8 +20,16 @@ import configuration from './config/configuration';
       envFilePath: ['.env.local', '.env'],
       cache: true,
     }),
+    // Infrastructure
     AuthModule,
     DatabaseModule,
+    KafkaModule,
+
+    // Modules
+    EmailModule,
+    PaymentModule,
+    OrderModule,
+    AnalyticModule,
   ],
   controllers: [AppController],
   providers: [AppService],
